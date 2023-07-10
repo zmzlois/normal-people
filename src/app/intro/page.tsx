626a848e42520f8693f5c885cbb3ff277857a7b3
Zhaomian Zhao <loisisar@outlook.com>
2023-07-10 04:25:59 +0100

    responsiveness framer motion

--- src/app/intro/page.tsx
+++ src/app/intro/page.tsx
@@ -10,27 +10,30 @@ import { useInView } from "react-intersection-observer"
 
 function Resume() {
     
+    const isMobile = window.innerWidth < 768 
     const { ref:firstref, inView: firstinView } = useInView({
-    threshold: 0.1, // Trigger the animation when the element is 50% in view
+    threshold: isMobile ? 0.5 : 1, // Trigger the animation when the element is 50% in view
     triggerOnce: true, // Only trigger the animation once
     })
      const { ref:secondref, inView: secondinView } = useInView({
-    threshold: 0.15, // Trigger the animation when the element is 50% in view
+    threshold: isMobile ? 0.08: 0.16, // Trigger the animation when the element is 50% in view
     triggerOnce: true, // Only trigger the animation once
   })
     return (
       
-        <div className="flex flex-col justify-center w-screen h-auto px-8 py-10 space-y-10 border border-blue-400 bg-slate-950" ref={firstref}>
+        <div className="flex flex-col justify-center w-screen h-auto px-8 py-10 space-y-10 border border-blue-400 bg-slate-950" >
+            <div ref={firstref}>
             <motion.div animate={
           firstinView ? { y: [20, 0], opacity: [0, 1] } : { y: [50], opacity: [0] }
         }
         transition={{ ease: "easeOut", duration: 1 }}
             >
                 <Head />
-            </motion.div>
+                </motion.div>
+                </div>
             <div ref={secondref}>
             <motion.div animate={
-          secondinView ? { y: [20, 0], opacity: [0, 1] } : { y: [50], opacity: [0] }
+          secondinView ? { y: [30, 0], opacity: [0, 1] } : { y: [50], opacity: [0] }
         }
         transition={{ ease: "easeOut", duration: 1 }} className="flex flex-col-reverse gap-4 sm:grid sm:grid-cols-3">
                 <Left />
