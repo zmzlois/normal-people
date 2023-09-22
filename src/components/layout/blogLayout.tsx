16261753333ff0a441eaf54ea10726c895791671
Lois <loisisar@outlook.com>
2023-09-22 20:17:19 +0100

    no time to love

--- src/components/layout/blogLayout.tsx
+++ src/components/layout/blogLayout.tsx
@@ -16,61 +16,6 @@ const navLink = [{
     }]
 export default function BlogHeader() {
     const path = usePathname()
-    const [didScroll, setDidScroll] = React.useState(false);
-    const [lastScrollTop, setLastScrollTop] = React.useState(0);
-    const delta = 5;
-    const [navbarHeight, setNavbarHeight] = React.useState(0);
-
-    React.useEffect(() => {
-        setNavbarHeight(document.getElementById('navi').offsetHeight);
-
-        const handleScroll = () => {
-            setDidScroll(true);
-        };
-
-        window.addEventListener('scroll', handleScroll);
-
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, []);
-
-    React.useEffect(() => {
-        const hasScrolled = () => {
-            const st = window.scrollY;
-
-            if (Math.abs(lastScrollTop - st) <= delta) return;
-
-            if (st > lastScrollTop && st > navbarHeight) {
-                // Scroll Down
-                document.getElementById('navi').style.top = '-60px';
-            } else {
-                // Scroll Up
-                if (st + window.innerHeight < document.documentElement.scrollHeight) {
-                    document.getElementById('navi').style.top = '0';
-                }
-            }
-
-            setLastScrollTop(st);
-        };
-
-        if (didScroll) {
-            hasScrolled();
-            setDidScroll(false);
-        }
-
-        const scrollInterval = setInterval(() => {
-            if (didScroll) {
-                hasScrolled();
-                setDidScroll(false);
-            }
-        }, 250);
-
-        return () => {
-            clearInterval(scrollInterval);
-        };
-    }, [didScroll, lastScrollTop]);
-
 
     return (
       <div className={cn("flex justify-between  py-4 sm:py-6 transition-all duration-500 backdrop-")} id={"navi"}>
