d28ff7f7ac913a8c4a25cb45f74b699b7e6b92ae
zmzlois <lois.z@comcord.vision>
2024-02-02 07:26:37 +0000

    cancel all animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,20 @@
-"use client"
-import Link from 'next/link'
-import React from 'react'
-import Particles from '../components/particles'
-import { useStore } from './store'
-import clsx from 'clsx'
+"use client";
+import Link from "next/link";
+import React from "react";
+// import Particles from '../components/particles'
+import { useStore } from "./store";
+import clsx from "clsx";
 const navigation = [
   //{ name: "Projects", href: "/projects" },
-  { name: 'Blog', href: '/blogs' },
-  { name: 'Contact', href: '/contact' },
-]
+  { name: "Blog", href: "/blogs" },
+  { name: "Contact", href: "/contact" },
+];
 
 export default function Home() {
-   const { visited } = useStore()
+  const { visited } = useStore();
   return (
     <div className="flex flex-col items-center justify-center w-screen h-screen px-6 overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black md:px-24">
-      <nav className={clsx("my-16", !visited && "animated-fade-in" )}>
+      <nav className={clsx("my-16", !visited && "animated-fade-in")}>
         <ul className="flex items-center justify-center gap-4">
           {navigation.map((item) => (
             <Link
@@ -27,44 +27,56 @@ export default function Home() {
           ))}
         </ul>
       </nav>
-      <div className={clsx("hidden w-screen   md:block bg-gradient-to-r  from-zinc-300/0 via-zinc-300/50 to-zinc-300/0", !visited && "animate-fade-left animate-glow  h-px")} />
-      <Particles
+      <div
+        className={clsx(
+          " w-screen   md:block bg-gradient-to-r  from-zinc-300/0 via-zinc-300/50 to-zinc-300/0"
+        )}
+      />
+      {/* <Particles
         className="absolute inset-0 -z-10 animate-fade-in"
         quantity={100}
-      />
+      /> */}
       {/* bg-gradient-to-tr from-yellow-200 via-orange-100 to-red-200 cursor-default text-edge-outline animate-title bg-clip-text */}
-      <h1 className={clsx("z-10 text-4xl font-extrabold tracking-tighter cursor-default duration-800 sm:text-6xl md:text-9xl  text-zinc-50 whitespace-nowrap text-edge-outline  bg-clip-text ", !visited && " animate-title ")}>
+      <h1
+        className={clsx(
+          "z-10 text-4xl font-extrabold tracking-tighter cursor-default duration-800 sm:text-6xl md:text-9xl  text-zinc-50 whitespace-nowrap text-edge-outline  bg-clip-text "
+        )}
+      >
         zmzlois
       </h1>
 
-      <div className={clsx("hidden w-screen md:block bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0 ", !visited && "animate-glow animate-fade-right h-px")} />
-      <div className={clsx("my-16 text-center ", !visited && "animate-fade-in")}>
+      {/* <div
+        className={clsx(
+          "hidden w-screen md:block bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0 ",
+          !visited && "animate-glow animate-fade-right h-px"
+        )}
+      /> */}
+      <div className={clsx("my-16 text-center ")}>
         <h2 className="text-sm font-light text-zinc-300">
           <b className="text-zinc-200">Also called Lois. </b>
-          <br /> Stumbling on building SaaS
-          
-          .<br className="block md:hidden" /> Tinker with{' '}
+          <br /> Stumbling on building SaaS .<br className="block md:hidden" />{" "}
+          Tinker with{" "}
           <a
             target="_blank"
             href="https://modernjs.dev/en"
             className="underline duration-500 hover:text-zinc-300"
           >
             Bytedance Infra
-          </a>{' '}
-          and{' '}
+          </a>{" "}
+          and{" "}
           <a
             target="_blank"
             href="https://zenstack.dev/"
             className="underline duration-500 hover:text-zinc-300"
           >
             ZenStack
-          </a>{' '}
+          </a>{" "}
           at night.
           <br className="block" />
           Occasionally detangle <span className="rotate-180">
             businesses
-          </span>{' '}
-          in my head for fun and turn them into{' '}
+          </span>{" "}
+          in my head for fun and turn them into{" "}
           <Link
             target="_blank"
             href="/blogs"
@@ -76,5 +88,5 @@ export default function Home() {
         </h2>
       </div>
     </div>
-  )
+  );
 }
--- src/app/store.tsx
+++ src/app/store.tsx
@@ -1,36 +1,34 @@
-"use client"
+"use client";
 import { create } from "zustand";
-import { usePathname, useRouter } from 'next/navigation';
+import { usePathname, useRouter } from "next/navigation";
 
 type StoreState = {
-  visited: boolean
-}
+  visited: boolean;
+};
 
 type StoreActions = {
-  setVisited: () => void
-}
+  setVisited: () => void;
+};
 
 export const useStore = create<StoreState & StoreActions>((set) => ({
   visited: false,
   setVisited: () => set((state) => ({ visited: true })),
-}))
-let number = 1
-import React from 'react'
+}));
+let number = 1;
+import React from "react";
 
-function Store({children}: {children: React.ReactNode}) {
-    const { setVisited } = useStore()
- 
-  const pathname = usePathname()
-  
+function Store({ children }: { children: React.ReactNode }) {
+  const { setVisited } = useStore();
+
+  const pathname = usePathname();
+
   React.useEffect(() => {
-    if (pathname === '/') {
-        setVisited()
-        console.log(number++)
+    if (pathname === "/") {
+      setVisited();
+      console.log(number++);
     }
-  }, [pathname, setVisited])
-  return (
-      <div>{children}</div>
-  )
+  }, [pathname, setVisited]);
+  return <>{children}</>;
 }
 
-export default Store
+export default Store;
