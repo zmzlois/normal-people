2524d8a2534fc2cde667c6e8dd8d2441dff172f1
zmzlois <lois.z@comcord.vision>
2024-02-02 07:30:34 +0000

    things that shouldn't be on client

--- src/app/intro/page.tsx
+++ src/app/intro/page.tsx
@@ -1,35 +1,18 @@
-import { Metadata } from 'next'
-import React from 'react'
-import Head from './head'
-import Left from './left'
-import Right from './right'
-
+import { Metadata } from "next";
+import React from "react";
+import Resume from "@/components/cv-thing/main";
 
 export const metadata: Metadata = {
-  title: 'Zhaomian Zhao Resume',
-  description: 'Updated on 2023-07-10',
-}
+  title: "Zhaomian Zhao Resume",
+  description: "Updated on 2023-07-10",
+};
 
-
-function Resume() {
-    
-   
-   
-    return (
-      
-        <div className="flex flex-col justify-center w-auto h-auto px-8 py-10 space-y-10 border border-blue-400 bg-slate-950" >
-           
-                <Head />
-                
-           
-            <div className="flex flex-col-reverse gap-4 sm:grid sm:grid-cols-3">
-                <Left />
-                <Right />
-                </div>
-             
-          
+function ResumePage() {
+  return (
+    <div className="flex flex-col justify-center  antialiased">
+      <Resume />
     </div>
-  )
+  );
 }
 
-export default Resume
+export default Resume;
