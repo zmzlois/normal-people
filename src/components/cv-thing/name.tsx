09462f80c1cb1c59d10742854deba9a0abc56afe
zmzlois <lois.z@comcord.vision>
2024-02-03 07:35:22 +0000

    edit left panel

--- src/components/cv-thing/name.tsx
+++ src/components/cv-thing/name.tsx
@@ -5,41 +5,62 @@ import { motion } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 import ChineseAnimate from "../chinese-animate";
 import EnglishAnimate from "../english-animate";
+import Balancer from "react-wrap-balancer";
+import {
+  GithubIcon,
+  LinkedinIcon,
+  MailIcon,
+  TwitterIcon,
+} from "../assets/icon";
 
 function Head() {
   return (
     <>
-      <div className="flex sticky h-screen top-0  sm:col-span-1 flex-col-reverse items-center content-center justify-center py-4   gap-y-6  sm:px-10 lg:gap-y-0 ">
-        <div className="flex flex-col justify-center px-2 text-center sm:px-0 ">
-          <h1 className=" text-xl font-bold sm:text-3xl text-slate-50">
-            {config.name}
-          </h1>
-          <h3 className="text-md font-semibold tracking-wide sm:text-xl text-slate-200">
-            {config.occupation}
-          </h3>
-          <p className="text-md font-medium tracking-wide sm:text-md text-slate-300">
-            {config.location}
-          </p>
-          <p className="text-xs font-normal tracking-wide sm:text-md text-slate-300">
-            Want to check this CV live? Click 👉🏻{" "}
-            <a
-              href={"https://normal-people.com/intro"}
-              className={
-                "decoration underline decoration-cyan-300 underline-offset-4"
-              }
-            >
-              here
-            </a>
-          </p>
-        </div>
-        <div className="pb-6">
-          {/* eslint-disable-next-line */}
-          {/* <ChineseAnimate /> */}
-          <EnglishAnimate />
-          {/* <img
+      <div className="flex flex-col lg:sticky lg:max-h-screen top-0 sm:col-span-1  items-start content-center py-20   sm:pl-40 sm:ml-20">
+        <div className="flex flex-col container justify-between mx-auto h-full px-20">
+          <div>
+            <div className="pb-6 -ml-10">
+              {/* eslint-disable-next-line */}
+              {/* <ChineseAnimate /> */}
+              <EnglishAnimate />
+              {/* <img
             className="w-20 h-20  rounded-full sm:w-36 sm:h-36 hover:cursor-pointer "
             src={config.avatar}
           /> */}
+            </div>
+            <div className=" -mt-20 text-start">
+              <h1 className=" text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl">
+                {config.name}
+              </h1>
+              <h3 className="mt-3 text-lg font-medium tracking-tight text-slate-200 sm:text-xl">
+                {config.occupation}
+              </h3>
+              {/* <p className="text-md font-medium tracking-tight sm:text-md text-slate-200">
+                {config.location}
+              </p> */}
+              <Balancer className="mt-4 tracking-wide text-slate-400 max-w-xs leading-normal">
+                I don't exist to use a framework. I exist to solve problems.
+              </Balancer>
+            </div>
+          </div>
+          <ul className="ml-1 mt-8 flex items-center" aria-label="Social media">
+            {socials.map((item) => (
+              <li className="ml-1 mt-8 flex items-center">
+                <a
+                  key={item.name}
+                  href={item.href}
+                  aria-label={item.aria}
+                  title={item.name}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="mx-3 text-sm font-light  block   hover:cursor-pointer"
+                >
+                  <span className="sr-only">{item.name}</span>
+                  {item.icon}
+                </a>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </>
@@ -47,3 +68,35 @@ function Head() {
 }
 
 export default Head;
+
+const socials = [
+  {
+    name: "Github",
+    href: "https://github.com/zmzlois",
+    icon: (
+      <GithubIcon className="w-6 h-6 text-slate-400 hover:text-slate-100 transition-colors duration-400" />
+    ),
+    aria: "GitHub (opens in a new tab)",
+  },
+  {
+    name: "Linkedin",
+    href: "https://www.linkedin.com/in/loiszhao/",
+    icon: (
+      <LinkedinIcon className="w-6 h-6 text-slate-400 hover:text-slate-100 transition-colors duration-400" />
+    ),
+  },
+  {
+    name: "Mail",
+    href: "mailto:loisisar@outlook.com",
+    icon: (
+      <MailIcon className="w-6 h-6 text-slate-400 hover:text-slate-100 transition-colors duration-400" />
+    ),
+  },
+  {
+    name: "Twitter",
+    href: "https://twitter.com/zmzlois",
+    icon: (
+      <TwitterIcon className="w-6 h-6 text-slate-400 hover:text-slate-100 transition-colors duration-400" />
+    ),
+  },
+];
