dfb70841d634712fb3b532d476006f1aba4a35e9
zmzlois <lois.z@comcord.vision>
2024-02-04 13:18:56 +0000

    remove redudant content

--- src/components/cv-thing/main.tsx
+++ src/components/cv-thing/main.tsx
@@ -1,18 +1,13 @@
 import React from "react";
-import Head from "./name";
-import Left from "../../components/cv-thing/left";
+import Left from "./name";
+// import Left from "../../components/cv-thing/left";
 import Right from "../../components/cv-thing/right";
 
 function Resume() {
   return (
-    <div className="flex flex-col sm:grid sm:grid-cols-2 justify-center  ">
-      <Head />
-
-      {/* <div className="flex flex-col-reverse gap-4 sm:grid sm:grid-cols-3"> */}
-
+    <div className="flex flex-col sm:grid sm:grid-cols-2 justify-center">
       <Left />
       <Right />
-      {/* </div> */}
     </div>
   );
 }
