fd1306dbe0cc654afadfeb12950c91e9be00d0b3
zmzlois <lois.z@comcord.vision>
2024-02-04 11:16:31 +0000

    glowing card borders?

--- src/components/cv-thing/right.tsx
+++ src/components/cv-thing/right.tsx
@@ -16,58 +16,64 @@ function Right() {
       <div className="space-y-8 py-20">
         {Companies.map((item: CompanyType, index) => (
           <li className="mb-12" key={index}>
-            <div className="group relative overflow-hidden grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
-              <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg" />
-              <header className="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2">
-                {item.time}
-              </header>
-              <a href={item.slug} className="z-10 sm:col-span-6">
-                <h3 className="font-medium leading-snug text-slate-200">
-                  <div>
-                    <div className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-blue-300 focus-visible:text-blue-300 group/link text-base">
-                      <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-5 md:-inset-y-4 lg:block" />
-                      <span>
-                        {item.title} ·{" "}
-                        <span className="inline-block">
-                          {item.name}
-                          <DoubleRightArrow className=" lg:ml-4 lg:mb-[0.16rem] inline-block text-slate-200 h-4 w-4 group-hover:-rotate-45 group-hover:scale-125 group-hover:translate-x-2 group-hover:skew-y-2 group-hover:transform-all group-hover:transition-all group-hover:text-sky-300" />
+            <div className="group relative rounded-xl bg-zinc-900 overflow-hidden px-[0.6px] pb-[1.02px] pt-[0.6px]">
+              <div className="absolute glow rotate-45 inset-0 w-[100px] h-[100px] z-0 hidden transition lg:-inset-x-6 lg:hidden lg:group-hover:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg" />
+              <div className="z-10 px-5 relative lg:hover:ring ring-slate-500/50 rounded-xl py-6 bg-zinc-900 grid  transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:hover:bg-zinc-800 lg:group-hover/list:opacity-50 lg:group-hover/list:bg-cyan-800">
+                <header className="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2">
+                  {item.time}
+                </header>
+                <a href={item.slug} className="z-10 sm:col-span-6">
+                  <h3 className="font-medium leading-snug text-slate-200">
+                    <div>
+                      <div className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-cyan-300 focus-visible:text-cyan-300 group/link text-base">
+                        <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-5 md:-inset-y-4 lg:block" />
+                        <span>
+                          {item.title} ·{" "}
+                          <span className="inline-block">
+                            {item.name}
+                            <DoubleRightArrow className=" lg:ml-4 lg:mb-[0.16rem] inline-block text-slate-200 h-4 w-4 group-hover:-rotate-45 group-hover:scale-125 group-hover:translate-x-2 group-hover:skew-y-2 group-hover:transform-all group-hover:transition-all group-hover:text-sky-300" />
+                          </span>
                         </span>
-                      </span>
+                      </div>
                     </div>
-                  </div>
-                </h3>
-              </a>
-              <p className="z-10 mt-1 text-md tracking-wide text-slate-400 sm:col-span-2 sm:col-start-3 sm:col-end-8">
-                {item.context}
-              </p>
-              <ul className="z-10 mt-1 flex gap-2 text-sm tracking-wide text-slate-500 sm:col-span-2 sm:col-start-3 sm:col-end-8">
-                {item.links &&
-                  Object.values(item.links).map((res, index) => (
-                    <li
-                      key={index}
-                      className="text-slate-200  px-3 rounded-full py-1"
-                    >
-                      <a
-                        href={res.url}
-                        className="hover:text-sky-300 duration-300 transition-color flex items-center space-x-1"
+                  </h3>
+                </a>
+                <p className="z-10 mt-1 text-md tracking-wide text-slate-400 sm:col-span-2 sm:col-start-3 sm:col-end-8">
+                  {item.context}
+                </p>
+                <ul className="z-10 mt-1 flex gap-2 text-sm tracking-wide text-slate-500 sm:col-span-2 sm:col-start-3 sm:col-end-8">
+                  {item.links &&
+                    Object.values(item.links).map((res, index) => (
+                      <li
+                        key={index}
+                        className="text-slate-200  px-3 rounded-full py-1"
                       >
-                        <LinkTwo className="inline-block" />
-                        <span>{res.name}</span>
-                      </a>
-                    </li>
-                  ))}
-              </ul>
-              <ul className="z-10 flex-wrap mt-1 gap-2 flex  sm:col-span-2 sm:col-start-3 sm:col-end-8">
-                {item.tags &&
-                  item.tags.map((res, index) => (
-                    <li
-                      key={index}
-                      className="text-cyan-200 text-xs font-light tracking-wider bg-cyan-700 px-3 rounded-full py-1"
-                    >
-                      <Balancer>{res}</Balancer>
-                    </li>
-                  ))}
-              </ul>
+                        <a
+                          href={res.url}
+                          aria-label={`${res.aria ?? ""} (Open in new tab)`}
+                          title={res.title}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className="hover:text-sky-200 duration-300 transition-color flex items-center space-x-1"
+                        >
+                          <LinkTwo className="inline-block" />
+                          <span>{res.name}</span>
+                        </a>
+                      </li>
+                    ))}
+                </ul>
+                <ul className="z-10 flex-wrap mt-1 gap-2 flex  sm:col-span-2 sm:col-start-3 sm:col-end-8">
+                  {item.tags &&
+                    item.tags.map((res, index) => (
+                      <li
+                        key={index}
+                        className="text-cyan-200 text-xs font-light tracking-wider bg-cyan-700 px-3 rounded-full py-1"
+                      >
+                        <Balancer>{res}</Balancer>
+                      </li>
+                    ))}
+                </ul>
+              </div>
             </div>
           </li>
         ))}
