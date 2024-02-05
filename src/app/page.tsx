e284a7ae452dbcd60ef53204a12b15b2048459af
zmzlois <lois.z@comcord.vision>
2024-02-05 08:07:31 +0000

    add experience list backdrop blur

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ const navigation = [
 export default function Home() {
   const { visited } = useStore();
   return (
-    <div className="flex flex-col items-center justify-center w-screen h-screen px-6 overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black md:px-24">
+    <div className="flex flex-col items-center justify-center w-screen h-screen px-6 overflow-hidden bg-zinc-900 md:px-24">
       <nav className={clsx("my-16", !visited && "animated-fade-in")}>
         <ul className="flex items-center justify-center gap-4">
           {navigation.map((item) => (
