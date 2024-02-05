c53839d8f5cd88373cf2faf2226567bc37db6825
zmzlois <lois.z@comcord.vision>
2024-02-05 21:24:36 +0000

    move view cv to main tree

--- src/components/cv-thing/right.tsx
+++ src/components/cv-thing/right.tsx
@@ -7,18 +7,25 @@ import clsx from "clsx";
 import { Balancer } from "react-wrap-balancer";
 
 import { About } from "./about";
-
+import { TopRightArrow } from "../assets/icon";
 import { Cards } from "./cards";
 
 function Right() {
   return (
-    <div className="flex flex-col sm:col-span-1 mx-5 sm:mx-0 py-4 sm:mr-10 sm:pr-20">
+    <div className="flex flex-col lg:col-span-1 mx-5  py-4 lg:pr-20">
       <About />
       <div id="body" className="space-y-4 py-12">
-        <div className="pb-36">
+        <div className="">
           <Cards title="experience" items={Companies} />
         </div>
-        <div className="pb-36">
+        <a
+          href="#cv"
+          className="text-sky-100 hover:text-cyan-300 hover:transition-all group px-4 py-4 "
+        >
+          <span className="">View full resume</span>{" "}
+          <TopRightArrow className="inline-block hover:transition-all duration-250 group-hover:skew-x-2 group-hover:scale-[1.12]" />
+        </a>
+        <div className="py-36">
           <Cards title="projects" items={Projects} />
         </div>
       </div>
