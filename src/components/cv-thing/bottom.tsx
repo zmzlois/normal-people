f89c36b3ec24af36b158ad8bdbcfb6f7e2089b4f
zmzlois <lois.z@comcord.vision>
2024-02-06 10:06:07 +0000

    bottom text wording

--- src/components/cv-thing/bottom.tsx
+++ src/components/cv-thing/bottom.tsx
@@ -3,7 +3,7 @@ export const BottomContent = () => {
     <>
       <span className="text-slate-400 text-sm font-light">
         Built with <a href="https://nextjs.org">Next.js</a> and{" "}
-        <a href="https://tailwindcss.com">Tailwind CSS</a>, deployed with{" "}
+        <a href="https://tailwindcss.com">Tailwind CSS</a>, deployed on{" "}
         <a href="https://vercel.com">Vercel</a>.
       </span>
     </>
