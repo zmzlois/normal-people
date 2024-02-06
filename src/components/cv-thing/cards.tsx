6208a144a54c8e1853e69e57e6ad7df86160628b
zmzlois <lois.z@comcord.vision>
2024-02-06 01:28:00 +0000

    animation only happen on link hover

--- src/components/cv-thing/cards.tsx
+++ src/components/cv-thing/cards.tsx
@@ -13,10 +13,10 @@ export const Cards = ({
   return (
     <>
       <h2>{title.toUpperCase()}</h2>
-      <ol className="group/list" id={title}>
+      <ol className="group/list">
         {items.map((item: any, index: number) => (
           <li className="mb-6 list-none" key={index}>
-            <a href={item.slug}>
+            <div>
               <div className="group relative rounded-xl bg-zinc-900 overflow-hidden px-[0.6px] pb-[1.02px] pt-[1px] lg:hover:!opacity-100 transition-all lg:group-hover/list:opacity-30">
                 <div
                   id="glow"
@@ -34,14 +34,17 @@ export const Cards = ({
                     </header>
                   )}
                   {item.image && (
-                    <header className="z-10col-span-1  mr-4 sm:col-span-2 sm:row-start-2 sm:row-end-3 flex ">
+                    <header className="hidden z-10 col-span-1  mr-4 sm:col-span-2 sm:row-start-2 sm:row-end-3 sm:flex ">
                       <img
-                        className="object-cover rounded-md border-[1px] border-cyan-950 aspect-video "
+                        className="object-cover rounded-md w-60 border-[1px] border-cyan-950 aspect-video "
                         src={item.image}
                       ></img>
                     </header>
                   )}
-                  <div className="z-10  sm:col-span-6 sm:col-start-3">
+                  <a
+                    href={item.slug}
+                    className="z-10  sm:col-span-6 sm:col-start-3"
+                  >
                     <h3 className="sm:font-medium font-normal leading-snug  hover:text-cyan-300 group-hover:!text-cyan-300 group-hover/list:text-cyan-300 focus-visible:text-cyan-300  text-slate-200 text-base tracking-wide  pt-4 lg:pt-0 pb-4 lg:pb-0">
                       <div className="inline-flex items-baseline group/link ">
                         {/* <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-5 md:-inset-y-4 lg:block" /> */}
@@ -53,7 +56,7 @@ export const Cards = ({
                             <DoubleRightArrow
                               className="
     ml-2 lg:ml-4 lg:mb-[0.16rem] inline-block text-slate-200 sm:h-4 sm:w-4 h-3 w-3
-    lg:group-hover/list:-rotate-45 lg:group-hover/list:scale-125 lg:group-hover/list:translate-x-1 lg:group-hover/list:skew-y-1
+    lg:group-hover/link:-rotate-45 lg:group-hover/link:scale-125 lg:group-hover/link:translate-x-1 lg:group-hover/link:skew-y-1
     lg:group-hover/list:transform-all lg:group-hover/list:transition-all lg:group-hover/link:text-sky-300
     transition-all hover:-rotate-45 hover:scale-125 hover:translate-x-1 hover:skew-y-1
     hover:transform-all hover:transition-all hover:text-sky-300 group-hover/list:text-sky-300
@@ -63,7 +66,7 @@ export const Cards = ({
                         </span>
                       </div>
                     </h3>
-                  </div>
+                  </a>
                   {item.description && (
                     <Balancer className="text-sm italic tracking-wide text-slate-400 sm:col-start-3 sm:col-end-10 ">
                       {item.description}
@@ -72,7 +75,7 @@ export const Cards = ({
                   {item.context && (
                     <Balancer
                       className={cn(
-                        "z-0 sm:py-1   py-3 sm:text-md text-sm tracking-wide group-hover/list:text-slate-200 text-slate-300 sm:col-span-2 sm:col-start-3 sm:col-end-10",
+                        "z-0 sm:py-1   py-3 leading-normal font-light  text-md tracking-wide group-hover/list:text-slate-200 text-slate-400 sm:col-span-2 sm:col-start-3 sm:col-end-10",
                         { "sm:row-start-2": !item.description }
                       )}
                     >
@@ -104,13 +107,21 @@ export const Cards = ({
                         ))}
                     </ul>
                   )}
+                  {item.image && (
+                    <header className="sm:hidden py-2 z-10 col-span-1  mr-4 sm:col-span-2 sm:row-start-2 sm:row-end-3 flex ">
+                      <img
+                        className="object-cover rounded-md w-60 border-[1px] border-cyan-950 aspect-video "
+                        src={item.image}
+                      ></img>
+                    </header>
+                  )}
                   {item.tags && (
                     <ul className="z-10 flex-wrap sm:row-span-1 pt-3 sm:pt-1 gap-2 flex  sm:col-start-3 sm:col-end-10">
                       {item.tags &&
                         item.tags.map((res: string, index: number) => (
                           <li
                             key={index}
-                            className="text-cyan-100 text-sm font-light tracking-wider bg-cyan-700 px-3 rounded-full py-1"
+                            className="text-cyan-200 text-xs font-light tracking-wider  bg-cyan-700/50 px-3 rounded-full py-1"
                           >
                             <p>{res}</p>
                           </li>
@@ -119,7 +130,7 @@ export const Cards = ({
                   )}
                 </div>
               </div>
-            </a>
+            </div>
           </li>
         ))}
       </ol>
