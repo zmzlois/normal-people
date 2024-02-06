58596cbe4de430580d7ed1a3068338b3cd22963a
zmzlois <lois.z@comcord.vision>
2024-02-06 03:56:46 +0000

    change meta data + add OG image

--- src/app/intro/layout.tsx
+++ src/app/intro/layout.tsx
@@ -1,5 +1,23 @@
 import BlogHeader from "@/components/layout/blogLayout";
 import "../../styles/cv.css";
+import type { Metadata } from "next";
+const date = new Date();
+export const metadata: Metadata = {
+  title: "Lois Zhao - Portfolio",
+  description: `Updated on ${date}`,
+  openGraph: {
+    type: "website",
+    url: "https://normal-people.com",
+    title: "Lois Zhao - Portfolio",
+    description: `Updated on ${date}`,
+    siteName: "Lois Zhao - Portfolio",
+    images: [
+      {
+        url: "https://normal-people.com/og-image.png",
+      },
+    ],
+  },
+};
 export default function Layout({
   children, // will be a page or nested layout
 }: {
