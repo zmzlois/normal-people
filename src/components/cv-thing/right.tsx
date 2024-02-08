9bc3392c621ea69157f087ba67a3b26dbf7fe237
zmzlois <lois.z@comcord.vision>
2024-02-08 11:48:21 +0000

    add writing blog part

--- src/components/cv-thing/right.tsx
+++ src/components/cv-thing/right.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { Volunteer, Education, Companies, Projects } from "./config";
+import { Volunteer, Education, Companies, Projects, Writing } from "./config";
 
 import type { VolunteerType, EducationType } from "./config";
 import clsx from "clsx";
@@ -19,13 +19,16 @@ function Right() {
       <div id="body" className="space-y-4">
         <div className="pt-16" id="experience">
           <Cards items={Companies} title="experience" />
+        </div>
+        <div>
+          {" "}
           <Link
             href="/cv_lois_zhao_06022024.pdf"
             target="_blank"
             locale={false}
             download={false}
             rel="noopener noreferrer"
-            className="text-sky-100 text-lg w-auto hover:text-cyan-300 hover:transition-all group sm:py-4 py-1 "
+            className="text-sky-100 text-lg relative w-auto hover:text-cyan-300 hover:transition-all group sm:py-4 py-2 "
           >
             <span className="">View full resume</span>{" "}
             <TopRightArrow className="inline-block transition-all duration-250 group-hover:skew-x-2 group-hover:-translate-y-1 group-hover:scale-[1.12]" />
@@ -34,6 +37,9 @@ function Right() {
 
         <div className="py-16" id="projects">
           <Cards items={Projects} title="projects" />
+        </div>
+        <div className="py-16" id="writing">
+          <Cards items={Writing} title="writing" />
         </div>
 
         <BottomContent />
