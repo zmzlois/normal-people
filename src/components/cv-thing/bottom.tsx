fd715def1e824ab2f3a0e7b34f6df80576146f88
zmzlois <lois.z@comcord.vision>
2024-02-08 11:46:06 +0000

    style: a tag styling

--- src/components/cv-thing/bottom.tsx
+++ src/components/cv-thing/bottom.tsx
@@ -1,12 +1,13 @@
+import Balancer from "react-wrap-balancer";
+
 export const BottomContent = () => {
   return (
     <div className="lg:text-end">
-      <p className="text-slate-400 items-baseline content-center  text-sm font-light">
-        Built with &nbsp;<a href="https://nextjs.org">Next.js</a>&nbsp;and
-        &nbsp;<a href="https://tailwindcss.com">Tailwind CSS</a>, deployed
-        on&nbsp;
+      <Balancer className="text-slate-400 items-baseline content-center  font-light">
+        Built with <a href="https://nextjs.org">Next.js</a>&nbsp;and{" "}
+        <a href="https://tailwindcss.com">Tailwind CSS</a>, deployed on{" "}
         <a href="https://vercel.com">Vercel</a>.
-      </p>
+      </Balancer>
     </div>
   );
 };
