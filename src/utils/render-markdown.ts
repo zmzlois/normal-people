ece2c2d610ece100054e7f15c625ed0ad2b0119c
zmzlois <lois.z@comcord.vision>
2024-03-10 23:57:11 +0000

    testing mdx

---
+++ src/utils/render-markdown.ts
@@ -0,0 +1,23 @@
+
+import rehypeHighlight from "rehype-highlight";
+import rehypeSlug from "rehype-slug";
+import rehypeStringify from "rehype-stringify";
+import remarkRehype from "remark-rehype";
+import remarkParse from "remark-parse";
+import { unified } from "unified";
+import hljs from "highlight.js";
+import protobuf from "highlight.js/lib/languages/protobuf";
+import { common } from "lowlight";
+
+hljs.registerLanguage("proto", protobuf);
+
+export async function renderMarkdown(markdown: string): Promise<string> {
+    return unified()
+        .use(remarkParse)
+        .use(remarkRehype)
+        .use(rehypeHighlight, { languages: { ...common, proto: protobuf } })
+        .use(rehypeSlug)
+        .use(rehypeStringify)
+        .process(markdown)
+        .then((res) => res.toString());
+}
