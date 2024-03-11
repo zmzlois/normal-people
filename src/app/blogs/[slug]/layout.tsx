9a6e94a4d19ff2b8d1c5ae8cf1ad0b89ff8cc80e
zmzlois <lois.z@comcord.vision>
2024-03-11 19:49:50 +0000

    does this work?

--- src/app/blogs/[slug]/layout.tsx
+++ src/app/blogs/[slug]/layout.tsx
@@ -14,6 +14,10 @@ function layout({
   const slug = params.slug;
   const blog = allBlogs.find((blog) => blog.slug === slug);
 
+  const title = blog ? `${blog.title}` : "Blog";
+
+  const description = blog ? blog.description : "By zmzlois";
+
   if (!blog) {
     throw new Error(`Post not found for slug: ${params.slug}`, notFound());
   }
@@ -21,11 +25,8 @@ function layout({
     <>
       <Head>
         <meta name="description" content={blog.description}></meta>
-        <meta
-          name="og:title"
-          content={`${blog.title} - Blog by ${blog.author}`}
-        />
-        <meta name="og:description" content={`${blog.description}`} />
+        <meta name="og:title" content={title} />
+        <meta name="og:description" content={description} />
         <meta
           name="og:image"
           content={
@@ -38,10 +39,22 @@ function layout({
             }/api/og`
           }
         />
-
+        <meta property="twitter:card" content="summary_large_image"></meta>
         <meta property="og:image:type" content="<generated>" />
         <meta property="og:image:width" content="<generated>" />
         <meta property="og:image:height" content="<generated>" />
+        <meta
+          property="twitter:image"
+          content={
+            // Because OG images must have a absolute URL, we use the
+            // `VERCEL_URL` environment variable to get the deployment’s URL.
+            // More info:
+            // https://vercel.com/docs/concepts/projects/environment-variables
+            `${
+              process.env.VERCEL_URL ? "https://" + process.env.VERCEL_URL : ""
+            }/api/og`
+          }
+        ></meta>
         <meta
           name="twitter:image"
           content={
@@ -57,6 +70,8 @@ function layout({
         <meta name="twitter:image:type" content="<generated>" />
         <meta name="twitter:image:width" content="<generated>" />
         <meta name="twitter:image:height" content="<generated>" />
+        <meta property="twitter:title" content={title}></meta>
+        <meta property="twitter:description" content={description}></meta>
       </Head>
       <div className="container mx-auto prose dark:prose-invert">
         {children}
