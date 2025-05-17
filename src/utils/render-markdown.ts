1780c51bc4d1f061056934693ffefd12e3dc75c0
Lois Zhao <lois.z@comcord.vision>
2025-05-17 02:57:20 +0100

    fix local build

--- src/utils/render-markdown.ts
+++ src/utils/render-markdown.ts
@@ -1,4 +1,3 @@
-
 import rehypeHighlight from "rehype-highlight";
 import rehypeSlug from "rehype-slug";
 import rehypeStringify from "rehype-stringify";
@@ -12,12 +11,15 @@ import { common } from "lowlight";
 hljs.registerLanguage("proto", protobuf);
 
 export async function renderMarkdown(markdown: string): Promise<string> {
-    return unified()
-        .use(remarkParse)
-        .use(remarkRehype)
-        .use(rehypeHighlight, { languages: { ...common, proto: protobuf } })
-        .use(rehypeSlug)
-        .use(rehypeStringify)
-        .process(markdown)
-        .then((res) => res.toString());
+  return (
+    unified()
+      .use(remarkParse)
+      .use(remarkRehype)
+      // @ts-expect-error type mismatch
+      .use(rehypeHighlight, { languages: { ...common, proto: protobuf } })
+      .use(rehypeSlug)
+      .use(rehypeStringify)
+      .process(markdown)
+      .then((res) => res.toString())
+  );
 }
